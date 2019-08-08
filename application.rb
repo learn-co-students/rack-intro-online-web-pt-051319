e91db812f07c67ed6d82a6c1367b5756254e2f88
ASHOK MAHARJAN <ashok997@gmail.com>
2019-08-08 16:26:51 +0000

    Done.

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World. My name is Automan!!"
+    resp.write "Hello, my name is Automan!!"
     resp.finish
   end
 
